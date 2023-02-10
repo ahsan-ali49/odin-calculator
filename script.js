535a24af38e2b0cbd90bef95df8ed531fe28672c
ahsan-ali49 <ahsanali.bscssef20@iba-suk.edu.pk>
2023-02-10 15:42:45 +0500

    added backspace and completed

--- script.js
+++ script.js
@@ -46,26 +46,53 @@ function display(btn) {
   const displayDiv = document.querySelector("#screen");
   if (btn.id == "clr") {
     displayDiv.textContent = "";
+    numA = 1;
+    numB = 1;
+    operator = "";
     times = 0;
   } else if (btn.className == "nmbr") {
     numB = numB * times + +btn.textContent;
     times = 10;
-    // console.log("Number: " + numB);
     console.log(numB);
     displayDiv.textContent += btn.textContent;
   } else if (btn.className == "opr") {
-    operator = btn.textContent;
-    console.log(operator);
-    displayDiv.textContent += btn.textContent;
-    numA = numB;
-    numB = 1;
-    times = 0;
-  } else if (btn.id == "equal") {
+    if (operator == "") {
+      operator = btn.textContent;
+      console.log(operator);
+      displayDiv.textContent += btn.textContent;
+      numA = numB;
+      numB = 1;
+      times = 0;
+    } else {
+      numB = operate(numA, numB, operator);
+      operator = btn.textContent;
+      displayDiv.textContent = numB + btn.textContent;
+      numA = numB;
+      numB = 1;
+      times = 0;
+    }
+  } else if (btn.id == "equal" && operator != "") {
     console.log(
       "NumA: " + numA + "  numB: " + numB + "  operator: " + operator
     );
     numB = operate(numA, numB, operator);
     displayDiv.textContent = numB;
+    operator = "";
+  }
+}
+
+function removeElement(backspace) {
+  const displayDiv = document.querySelector("#screen");
+  let lastNum = displayDiv.textContent[displayDiv.textContent.length - 1];
+  console.log(lastNum);
+  displayDiv.textContent = displayDiv.textContent.substring(
+    0,
+    displayDiv.textContent.length - 1
+  );
+  if (times != 0) {
+    numB = numB - +lastNum;
+    numB = numB / times;
+    console.log("Hello NumB: " + numB);
   }
 }
 
@@ -85,3 +112,6 @@ clr.addEventListener("click", () => display(clr));
 
 const eqaul = document.querySelector("#equal");
 eqaul.addEventListener("click", () => display(eqaul));
+
+const backspace = document.querySelector("#backspace");
+backspace.addEventListener("click", () => removeElement(backspace));
